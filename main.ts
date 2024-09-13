98e19071648db1c817cb817f88b4a34bb0591ec7
falentio <kevin@falentio.com>
2024-09-13 17:59:35 +0700

    init

---
+++ main.ts
@@ -0,0 +1,22 @@
+import "jsr:@std/dotenv/load"
+import { createClient } from "npm:@libsql/client@0.10.0"
+  const client = createClient({
+  url: "libsql://213-falentio.turso.io",
+  authToken: Deno.env.get("LIBSQL_TOKEN"),
+})
+
+
+await client.execute(`
+CREATE TABLE IF NOT EXISTS counter(
+  id text primary key,
+  n int not null
+);
+`)
+
+await client.execute(`
+INSERT INTO counter values('test', 0)`)
+
+Deno.serve(async () => {
+  const result = await client.execute(`update counter set n = n + 1 where id = 'test'`)
+  return Response.json(result)
+})
